b4b3f69c099b94b4284f399d4eb94f14728a207f
Zain Arisar <zainarisar44@gmail.com>
2024-09-19 02:25:17 +0500

    All files added ...

---
+++ src/BlogDetails.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useParams, useHistory } from "react-router-dom";
+import useFetch from "./useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isPending,
+  } = useFetch("http://localhost:8001/blogs/" + id);
+  const history = useHistory();
+
+  const handleClick = () => {
+    fetch("http://localhost:8001/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      history.go(-1);
+    });
+  };
+
+  return (
+    <div className="blog-details">
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
